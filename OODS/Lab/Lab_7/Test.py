c70ab53fe870c2ae1cad02509655a60e4a969378
Nattawut Chayauam <nattawutth011@gmail.com>
2023-09-18 16:15:48 +0700

    18-9-23

--- OODS/Lab/Lab_7/Test.py
+++ OODS/Lab/Lab_7/Test.py
@@ -1,178 +1,97 @@
-class Queue:
-    def __init__(self, q = None):
-        if q == None:
-            self.item = []
-        else:
-            self.item = q
-    def enQueue(self, i):
-        self.item.append(i)
-    def deQueue(self):
-        return self.item.pop(0)
-    def isEmpty(self):
-        return self.item == []
-    def size(self):
-        return len(self.item)
+class TreeNode(object): 
+    def __init__(self, val): 
+        self.val = val 
+        self.left = None
+        self.right = None
+        self.height = 1
 
-class BST:
-    class Node:
-        def __init__(self, data, left = None, right = None):
-            self.data = data
-            self.left = None if left is None else left
-            self.right = None if right is None else right
-        
-        def __str__(self):
-            s = str(self.data)
-            return s
-    
-    def __init__(self, root = None):
-        self.root = None if root is None else root
-        
-    def add(self, data):
-        self.root = BST._add(self.root, data)
-        
+    def __str__(self):
+        return str(self.val)
 
-    def _add(root, data):
-        if root is None:
-            return BST.Node(data)
+class AVL_Tree(object):
+    def getHeight(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def getBalance(self, node):
+        if not node:
+            return 0
+        return self.getHeight(node.left) - self.getHeight(node.right)
+
+    def updateHeight(self, node):
+        if not node:
+            return
+        node.height = 1 + max(self.getHeight(node.left), self.getHeight(node.right))
+
+    def leftRotate(self, z):
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        self.updateHeight(z)
+        self.updateHeight(y)
+
+        return y
+
+    def rightRotate(self, y):
+        x = y.left
+        T2 = x.right
+
+        x.right = y
+        y.left = T2
+
+        self.updateHeight(y)
+        self.updateHeight(x)
+
+        return x
+
+    def insert(self, root, val):
+        if not root:
+            return TreeNode(val)
+
+        if val < root.val:
+            root.left = self.insert(root.left, val)
         else:
-            if data < root.data:
-                root.left = BST._add(root.left, data)
+            root.right = self.insert(root.right, val)
+
+        self.updateHeight(root)
+
+        balance = self.getBalance(root)
+
+        # Left Heavy
+        if balance > 1:
+            if val < root.left.val:
+                return self.rightRotate(root)
             else:
-                root.right = BST._add(root.right, data)
+                root.left = self.leftRotate(root.left)
+                return self.rightRotate(root)
+
+        # Right Heavy
+        if balance < -1:
+            if val > root.right.val:
+                return self.leftRotate(root)
+            else:
+                root.right = self.rightRotate(root.right)
+                return self.leftRotate(root)
+
         return root
+
+def printTree90(node, level=0):
+    if node is not None:
+        printTree90(node.right, level + 1)
+        print('     ' * level, node)
+        printTree90(node.left, level + 1)
+
+myTree = AVL_Tree()
+root = None
+
+data = input("Enter Input : ").split()
+for e in data:
+    print("insert :", e)
+    root = myTree.insert(root, int(e))
+    printTree90(root)
+    print("===============")
     
-    def leverOrder(self):
-        q = Queue()
-        q.enQueue(self.root)
-        while q.isEmpty() is not True :
-            n = q.deQueue()
-            print(n.data, end = ' ')
-            if n.left is not None:
-                q.enQueue(n.left)
-            if n.right is not None:
-                q.enQueue(n.right)
-        
-
-    def inOrder(self):
-        BST._inOrder(self.root)
-
-    def _inOrder(root):
-        if root is not None:
-            BST._inOrder(root.left)
-            print(root, end = ' ')
-            BST._inOrder(root.right)
-    
-    def preOrder(self):
-        BST._preOrder(self.root)
-
-    def _preOrder(root):
-        if root is not None:
-            print(root, end = ' ') 
-            BST._preOrder(root.left)
-            BST._preOrder(root.right)
-
-    def postOrder(self):
-        BST._postOrder(self.root)
-
-    def _postOrder(root):
-        if root is not None:
-            BST._postOrder(root.left)
-            BST._postOrder(root.right)
-            print(root, end = ' ')
-
-
-    def deleteNode(self, data):
-        self.root = BST._deleteNodeP(self.root, data)
-        
-    def _deleteNodeS(root : Node, key : int) :
-        if root is None : return root
-        if int(key) < int(root.data) :
-            root.left = BST._deleteNodeS(root.left, key)
-        elif int(key) > int(root.data) :
-            root.right = BST._deleteNodeS(root.right, key)
-        else :
-            if root.left is None or root.right is None :
-                root = root.left if root.right is None else root.right
-            else :
-                temp = root.right
-                while temp.left is not None :
-                    temp = temp.left
-                root.data = temp.data
-                root.right = BST._deleteNodeS(root.right, temp.data)
-        return root  
-
-    def _deleteNodeP(root : Node, key : int) :
-        if root is None : return root
-        if int(key) < int(root.data) :
-            root.left = BST._deleteNodeP(root.left, key)
-        elif int(key) > int(root.data) :
-            root.right = BST._deleteNodeP(root.right, key)
-        else :
-            if root.left is None or root.right is None :
-                root = root.left if root.right is None else root.right
-            else :
-                temp = root.left
-                while temp.right is not None :
-                    temp = temp.right
-                root.data = temp.data
-                print(f'p = {root.data}')
-                root.left = BST._deleteNodeP(root.left, temp.data)
-        return root  
-
-    def __str__(self) -> str:
-        lines = BST._build_tree_string(self.root, 0, False, "-")[0]
-        return "\n" + "\n".join((line.rstrip() for line in lines))
-    
-    def _build_tree_string( 
-        root: Node,
-        curr_index: int,
-        include_index: bool = False,
-        delimiter: str = "-") :
-        if root is None:
-            return [], 0, 0, 0
-        line1 = []
-        line2 = []
-        if include_index:
-            node_repr = "{}{}{}".format(curr_index, delimiter, root.data)
-        else:
-            node_repr = str(root.data)
-        new_root_width = gap_size = len(node_repr)
-        l_box, l_box_width, l_root_start, l_root_end = \
-            BST._build_tree_string(root.left, 2 * curr_index + 1, include_index, delimiter)
-        r_box, r_box_width, r_root_start, r_root_end = \
-            BST._build_tree_string(root.right, 2 * curr_index + 2, include_index, delimiter)
-        if l_box_width > 0:
-            l_root = (l_root_start + l_root_end) // 2 + 1
-            line1.append(" " * (l_root + 1))
-            line1.append("_" * (l_box_width - l_root))
-            line2.append(" " * l_root + "/")
-            line2.append(" " * (l_box_width - l_root))
-            new_root_start = l_box_width + 1
-            gap_size += 1
-        else:
-            new_root_start = 0
-        line1.append(node_repr)
-        line2.append(" " * new_root_width)
-        if r_box_width > 0:
-            r_root = (r_root_start + r_root_end) // 2
-            line1.append("_" * (r_root))
-            line1.append(" " * (r_box_width - r_root + 1))
-            line2.append(" " * r_root + "\\")
-            line2.append(" " * (r_box_width - r_root))
-            gap_size += 1
-        new_root_end = new_root_start + new_root_width - 1
-        gap = " " * gap_size
-        new_box = ["".join(line1), "".join(line2)]
-        for i in range(max(len(l_box), len(r_box))):
-            l_line = l_box[i] if i < len(l_box) else " " * l_box_width
-            r_line = r_box[i] if i < len(r_box) else " " * r_box_width
-            new_box.append(l_line + gap + r_line)
-        return new_box, len(new_box[0]), new_root_start, new_root_end
-    
-
-tree1 = BST()
-inp = input("Enter input : ")
-data = [int(e) for e in inp.split(" ")]
-for i in data :
-    tree1.add(i)
-print(tree1)
